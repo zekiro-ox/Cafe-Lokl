99799212cd6c5f12c9eb693e7723d8ff320e68e0
Rendelle Bertes <118584640+zekiro-ox@users.noreply.github.com>
2024-08-10 12:01:11 +0800

    Modded

--- src/EmployeeAccount.jsx
+++ src/EmployeeAccount.jsx
@@ -4,46 +4,109 @@ import Sidebar from "./Sidebar";
 
 const EmployeeAccount = () => {
   const [searchTerm, setSearchTerm] = useState("");
-
-  const employees = [
+  const [employees, setEmployees] = useState([
     {
+      id: "E001",
       name: "John Doe",
       email: "john.doe@example.com",
       position: "Manager",
-      profilePicture: "https://via.placeholder.com/150",
     },
     {
+      id: "E002",
       name: "Jane Smith",
       email: "jane.smith@example.com",
       position: "Barista",
-      profilePicture: "https://via.placeholder.com/150",
     },
     {
+      id: "E003",
       name: "Alice Johnson",
       email: "alice.johnson@example.com",
       position: "Cashier",
-      profilePicture: "https://via.placeholder.com/150",
     },
-    // Add more employees here
-  ];
+  ]);
+
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [newEmployee, setNewEmployee] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    position: "",
+  });
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [passwordError, setPasswordError] = useState("");
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleInputChange = (e) => {
+    setNewEmployee({
+      ...newEmployee,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddEmployee = () => {
+    if (newEmployee.password !== newEmployee.confirmPassword) {
+      setPasswordError("Passwords do not match.");
+      return;
+    }
+
+    const fullName = `${newEmployee.firstName} ${newEmployee.lastName}`;
+    const newId = `E${(employees.length + 1).toString().padStart(3, "0")}`;
+
+    setEmployees([
+      ...employees,
+      {
+        id: newId,
+        name: fullName,
+        email: newEmployee.email,
+        position: newEmployee.position,
+      },
+    ]);
+
+    setShowAddForm(false);
+    setNewEmployee({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      position: "",
+    });
+    setPasswordError("");
+  };
+
+  const handleEditEmployee = () => {
+    const updatedEmployees = employees.map((employee) =>
+      employee.id === selectedEmployee.id
+        ? { ...employee, ...selectedEmployee }
+        : employee
+    );
+    setEmployees(updatedEmployees);
+    setShowEditForm(false);
+    setSelectedEmployee(null);
+  };
+
   const filteredEmployees = employees.filter(
     (employee) =>
+      employee.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
       employee.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
       employee.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
       employee.position.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const handleEdit = (employee) => {
-    alert(`Edit functionality for ${employee.name} to be implemented`);
+    const [firstName, lastName] = employee.name.split(" ");
+    setSelectedEmployee({ ...employee, firstName, lastName });
+    setShowEditForm(true);
   };
 
   const handleDelete = (employee) => {
-    alert(`Delete functionality for ${employee.name} to be implemented`);
+    setEmployees(employees.filter((e) => e.id !== employee.id));
   };
 
   return (
@@ -64,9 +127,7 @@ const EmployeeAccount = () => {
               <FaSearch className="absolute top-3 left-2 text-gray-500" />
             </div>
             <button
-              onClick={() =>
-                alert("Add Account functionality to be implemented")
-              }
+              onClick={() => setShowAddForm(true)}
               className="flex items-center px-4 py-2 bg-brown-500 text-white rounded-lg hover:bg-brown-600 md:px-6 md:py-3 md:text-lg"
             >
               <FaPlus className="mr-2" /> Add Account
@@ -77,7 +138,7 @@ const EmployeeAccount = () => {
           <table className="min-w-full bg-white rounded-lg shadow-md">
             <thead className="bg-brown-500 text-white">
               <tr>
-                <th className="py-3 px-4 text-left">Profile Picture</th>
+                <th className="py-3 px-4 text-left">Employee ID</th>
                 <th className="py-3 px-4 text-left">Name</th>
                 <th className="py-3 px-4 text-left">Email</th>
                 <th className="py-3 px-4 text-left">Position</th>
@@ -87,13 +148,7 @@ const EmployeeAccount = () => {
             <tbody>
               {filteredEmployees.map((employee, index) => (
                 <tr key={index} className="border-b">
-                  <td className="py-3 px-4">
-                    <img
-                      src={employee.profilePicture}
-                      alt={employee.name}
-                      className="h-10 w-10 rounded-full object-cover"
-                    />
-                  </td>
+                  <td className="py-3 px-4">{employee.id}</td>
                   <td className="py-3 px-4">{employee.name}</td>
                   <td className="py-3 px-4">{employee.email}</td>
                   <td className="py-3 px-4">{employee.position}</td>
@@ -117,6 +172,202 @@ const EmployeeAccount = () => {
           </table>
         </div>
       </div>
+
+      {/* Add Employee Modal */}
+      {showAddForm && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+            <h2 className="text-xl font-bold mb-4">Add Employee</h2>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={newEmployee.firstName}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={newEmployee.lastName}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={newEmployee.email}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={newEmployee.password}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={newEmployee.confirmPassword}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+              {passwordError && (
+                <p className="text-red-500 text-sm mt-2">{passwordError}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Position
+              </label>
+              <select
+                name="position"
+                value={newEmployee.position}
+                onChange={handleInputChange}
+                className="mt-1 p-2 w-full border rounded-lg"
+              >
+                <option value="">Select Position</option>
+                <option value="Manager">Manager</option>
+                <option value="Barista">Barista</option>
+                <option value="Cashier">Cashier</option>
+              </select>
+            </div>
+            <div className="flex justify-end">
+              <button
+                onClick={handleAddEmployee}
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+              >
+                Add Employee
+              </button>
+              <button
+                onClick={() => setShowAddForm(false)}
+                className="ml-2 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Edit Employee Modal */}
+      {showEditForm && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+            <h2 className="text-xl font-bold mb-4">Edit Employee</h2>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                value={selectedEmployee.firstName}
+                onChange={(e) =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    firstName: e.target.value,
+                  })
+                }
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                value={selectedEmployee.lastName}
+                onChange={(e) =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    lastName: e.target.value,
+                  })
+                }
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={selectedEmployee.email}
+                onChange={(e) =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    email: e.target.value,
+                  })
+                }
+                className="mt-1 p-2 w-full border rounded-lg"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Position
+              </label>
+              <select
+                name="position"
+                value={selectedEmployee.position}
+                onChange={(e) =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    position: e.target.value,
+                  })
+                }
+                className="mt-1 p-2 w-full border rounded-lg"
+              >
+                <option value="Manager">Manager</option>
+                <option value="Barista">Barista</option>
+                <option value="Cashier">Cashier</option>
+              </select>
+            </div>
+            <div className="flex justify-end">
+              <button
+                onClick={handleEditEmployee}
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+              >
+                Save Changes
+              </button>
+              <button
+                onClick={() => setShowEditForm(false)}
+                className="ml-2 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
